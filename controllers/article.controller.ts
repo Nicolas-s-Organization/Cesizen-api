45cb83ac441901e54a798da968a621757e71a3fe
Nicolas Descarpentries <nicolas.descarp@outlook.fr>
2026-02-04 20:49:15 +0100

    feat: add getArticleById route

--- controllers/article.controller.ts
+++ controllers/article.controller.ts
@@ -14,3 +14,26 @@ export const getArticles = async (req: Request, res: Response) => {
         return res.status(500).json({ code: "INTERNAL_ERROR", message: "Erreur serveur" });
     }
 };
+
+
+export const getArticleById = async (req: Request, res: Response) => {
+    try {
+        const { articleId } = req.params;
+        if (!articleId || typeof articleId !== "string") {
+            throw new AppError("Identifiant de l'article invalide", "INVALID_ARTICLE_ID", 400);
+        }
+        
+        const user = await articleService.getArticleById(articleId);
+
+        res.status(200).json(user);
+    }
+    catch (error) {
+        console.error(error);
+
+        if (error instanceof AppError) {
+            return res.status(error.statusCode).json({ code: error.code, message: error.message });
+        }
+
+        return res.status(500).json({ code: "INTERNAL_ERROR", message: "Erreur serveur" });
+    }
+};
--- services/article.service.ts
+++ services/article.service.ts
@@ -3,10 +3,27 @@ import { AppError } from "../utils/error";
 
 
 export const getAllArticles = async () => {
-  return prisma.article.findMany({
-    include: {
-      user: true,
-      category : true
-    },
-  });
+    return prisma.article.findMany({
+        include: {
+            user: true,
+            category: true
+        },
+    });
+};
+
+
+export const getArticleById = async (id: string) => {
+    const article = await prisma.article.findUnique({
+        where: { id },
+        include: {
+            user: true,
+            category: true
+        },
+    });
+
+    if (!article) {
+        throw new AppError("Article introuvable", "USER_NOT_FOUND", 404);
+    }
+
+    return article;
 };
